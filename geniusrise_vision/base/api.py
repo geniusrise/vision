cf6a5e7c63b6ee6c456046735c9471198231398d
ixaxaar <root@ixaxaar.in>
2023-11-06 15:50:27 +0530

    add base classes

--- geniusrise_vision/base/api.py
+++ geniusrise_vision/base/api.py
@@ -12,3 +12,163 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from typing import Any, Dict, Optional
+
+import cherrypy
+from geniusrise import BatchInput, BatchOutput, State
+from geniusrise.logging import setup_logger
+from .bulk import ImageBulk
+
+
+class TextAPI(ImageBulk):
+    """
+    A class representing a Hugging Face API for generating text using a pre-trained language model.
+
+    Attributes:
+        model (Any): The pre-trained language model.
+        processor (Any): The processor used to preprocess input text.
+        model_name (str): The name of the pre-trained language model.
+        model_revision (Optional[str]): The revision of the pre-trained language model.
+        processor_name (str): The name of the processor used to preprocess input text.
+        processor_revision (Optional[str]): The revision of the processor used to preprocess input text.
+        model_class (str): The name of the class of the pre-trained language model.
+        processor_class (str): The name of the class of the processor used to preprocess input text.
+        use_cuda (bool): Whether to use a GPU for inference.
+        quantization (int): The level of quantization to use for the pre-trained language model.
+        precision (str): The precision to use for the pre-trained language model.
+        device_map (str | Dict | None): The mapping of devices to use for inference.
+        max_memory (Dict[int, str]): The maximum memory to use for inference.
+        torchscript (bool): Whether to use a TorchScript-optimized version of the pre-trained language model.
+        model_args (Any): Additional arguments to pass to the pre-trained language model.
+
+    Methods:
+        text(**kwargs: Any) -> Dict[str, Any]:
+            Generates text based on the given prompt and decoding strategy.
+
+        listen(model_name: str, model_class: str = "AutoModelForCausalLM", processor_class: str = "Autoprocessor", use_cuda: bool = False, precision: str = "float16", quantization: int = 0, device_map: str | Dict | None = "auto", max_memory={0: "24GB"}, torchscript: bool = True, endpoint: str = "*", port: int = 3000, cors_domain: str = "http://localhost:3000", username: Optional[str] = None, password: Optional[str] = None, **model_args: Any) -> None:
+            Starts a CherryPy server to listen for requests to generate text.
+    """
+
+    model: Any
+    processor: Any
+
+    def __init__(
+        self,
+        input: BatchInput,
+        output: BatchOutput,
+        state: State,
+    ):
+        """
+        Initializes a new instance of the TextAPI class.
+
+        Args:
+            input (BatchInput): The input data to process.
+            output (BatchOutput): The output data to process.
+            state (State): The state of the API.
+        """
+        super().__init__(input=input, output=output, state=state)
+        self.log = setup_logger(self)
+
+    def listen(
+        self,
+        model_name: str,
+        model_class: str = "AutoModel",
+        processor_class: str = "AutoProcessor",
+        use_cuda: bool = False,
+        precision: str = "float16",
+        quantization: int = 0,
+        device_map: str | Dict | None = "auto",
+        max_memory={0: "24GB"},
+        torchscript: bool = True,
+        endpoint: str = "*",
+        port: int = 3000,
+        cors_domain: str = "http://localhost:3000",
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        **model_args: Any,
+    ) -> None:
+        """
+        Starts a CherryPy server to listen for requests to generate text.
+
+        Args:
+            model_name (str): The name of the pre-trained language model.
+            model_class (str, optional): The name of the class of the pre-trained language model. Defaults to "AutoModelForCausalLM".
+            processor_class (str, optional): The name of the class of the processor used to preprocess input text. Defaults to "Autoprocessor".
+            use_cuda (bool, optional): Whether to use a GPU for inference. Defaults to False.
+            precision (str, optional): The precision to use for the pre-trained language model. Defaults to "float16".
+            quantization (int, optional): The level of quantization to use for the pre-trained language model. Defaults to 0.
+            device_map (str | Dict | None, optional): The mapping of devices to use for inference. Defaults to "auto".
+            max_memory (Dict[int, str], optional): The maximum memory to use for inference. Defaults to {0: "24GB"}.
+            torchscript (bool, optional): Whether to use a TorchScript-optimized version of the pre-trained language model. Defaults to True.
+            endpoint (str, optional): The endpoint to listen on. Defaults to "*".
+            port (int, optional): The port to listen on. Defaults to 3000.
+            cors_domain (str, optional): The domain to allow CORS requests from. Defaults to "http://localhost:3000".
+            username (Optional[str], optional): The username to use for authentication. Defaults to None.
+            password (Optional[str], optional): The password to use for authentication. Defaults to None.
+            **model_args (Any): Additional arguments to pass to the pre-trained language model.
+        """
+        self.model_name = model_name
+        self.model_class = model_class
+        self.processor_class = processor_class
+        self.use_cuda = use_cuda
+        self.quantization = quantization
+        self.precision = precision
+        self.device_map = device_map
+        self.max_memory = max_memory
+        self.torchscript = torchscript
+        self.model_args = model_args
+
+        if ":" in model_name:
+            model_revision = model_name.split(":")[1]
+            processor_revision = model_name.split(":")[1]
+            model_name = model_name.split(":")[0]
+            processor_name = model_name
+        else:
+            model_revision = None
+            processor_revision = None
+        self.model_name = model_name
+        self.model_revision = model_revision
+        self.processor_name = processor_name
+        self.processor_revision = processor_revision
+
+        self.model, self.processor = self.load_models(
+            model_name=self.model_name,
+            processor_name=self.processor_name,
+            model_revision=self.model_revision,
+            processor_revision=self.processor_revision,
+            model_class=self.model_class,
+            processor_class=self.processor_class,
+            use_cuda=self.use_cuda,
+            precision=self.precision,
+            quantization=self.quantization,
+            device_map=self.device_map,
+            max_memory=self.max_memory,
+            torchscript=self.torchscript,
+            **self.model_args,
+        )
+
+        def CORS():
+            cherrypy.response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+            cherrypy.response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+            cherrypy.response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+            cherrypy.response.headers["Access-Control-Allow-Credentials"] = "true"
+
+            if cherrypy.request.method == "OPTIONS":
+                cherrypy.response.status = 200
+                return True
+
+        cherrypy.config.update(
+            {
+                "server.socket_host": "0.0.0.0",
+                "server.socket_port": port,
+                "log.screen": False,
+                "tools.CORS.on": True,
+            }
+        )
+
+        cherrypy.tools.CORS = cherrypy.Tool("before_handler", CORS)
+        cherrypy.tree.mount(self, "/api/v1/", {"/": {"tools.CORS.on": True}})
+        cherrypy.tools.CORS = cherrypy.Tool("before_finalize", CORS)
+        cherrypy.engine.start()
+        cherrypy.engine.block()
