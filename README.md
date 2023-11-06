3ac499cf596824753a5280c34693023c0e243718
ixaxaar <root@ixaxaar.in>
2023-11-06 14:45:27 +0530

    initial

---
+++ README.md
@@ -0,0 +1,34 @@
+<h1 align="center">
+  <img src="./assets/logo_with_text.png" alt=logo" width="900"/>
+</h1>
+<h2 align="center">
+  <a style="color:#f34960" href="https://docs.geniusrise.ai">Documentation</a>
+</h2>
+
+<p align="center">
+  <img src="https://img.shields.io/github/actions/workflow/status/geniusrise/geniusrise-vision/pytest.yml?branch=master" alt="GitHub Workflow Status"/>
+  <img src="https://codecov.io/gh/geniusrise/geniusrise-vision/branch/main/graph/badge.svg?token=0b359b3a-f29c-4966-9661-a79386b3450d" alt="Codecov"/>
+  <img src="https://img.shields.io/github/license/geniusrise/geniusrise-vision" alt="Codecov"/>
+  <img src="https://img.shields.io/github/issues/geniusrise/geniusrise-vision" alt="Codecov"/>
+</p>
+
+---
+
+## <span style="color:#e667aa">About</span>
+
+<span style="color:#e4e48c">Geniusrise</span> is a modular, loosely-coupled
+AgentOps / MLOps framework designed for the era of Large Language Models,
+offering flexibility, inclusivity, and standardization in designing networks of
+AI agents.
+
+It seamlessly integrates tasks, state management, data handling, and model
+versioning, all while supporting diverse infrastructures and user expertise
+levels. With its plug-and-play architecture,
+<span style="color:#e4e48c">Geniusrise</span> empowers teams to build, share,
+and deploy AI agent workflows across various platforms.
+
+## <span style="color:#e667aa">Huggingface Bolts</span>
+
+This is a collection of vision components.
+
+Includes:
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="geniusrise-vision",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=requirements,
+    python_requires=">=3.10",
+    author="ixaxaar",
+    author_email="ixaxaar@geniusrise.ai",
+    description="Huggingface bolts for geniusrise",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/geniusrise/geniusrise-vision",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    keywords="mlops, llm, geniusrise, machine learning, data processing",
+    project_urls={
+        "Bug Reports": "https://github.com/geniusrise/geniusrise-vision/issues",
+        "Source": "https://github.com/geniusrise/geniusrise-vision",
+        "Documentation": "https://docs.geniusrise.ai/",
+    },
+    package_data={
+        "geniusrise": [],
+    },
+    extras_require={
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
+    },
+)
