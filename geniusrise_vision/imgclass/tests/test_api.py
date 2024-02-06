566a5845039d27dc6e0b73a526dcad09d1f64dcc
KeertanaSG <gkshivroop@email.com>
2024-02-06 14:15:04 +0530

    Made modifications to imgclass api tests

--- geniusrise_vision/imgclass/tests/test_api.py
+++ geniusrise_vision/imgclass/tests/test_api.py
@@ -18,20 +18,29 @@ import cherrypy
 import base64
 import io
 import torch
+import random
+import string
 from unittest.mock import patch, MagicMock
 from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from geniusrise import BatchInput, BatchOutput, InMemoryState, State
 from geniusrise_vision.imgclass.api import VisionClassificationAPI 
 from transformers import AutoModelForImageClassification, AutoProcessor
 
 # Fixtures
 @pytest.fixture
-def test_image():
-    # Create a simple RGB image for testing
-    img = Image.new('RGB', (100, 100), color='red')
+def base64_test_image():
+    # Create an image with random text
+    img = Image.new('RGB', (384, 384), color='white')
+    draw = ImageDraw.Draw(img)
+    text = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    draw.text((10, 10), text, fill='black')
+
+    # Convert the image to a Base64 string
     img_byte_arr = io.BytesIO()
     img.save(img_byte_arr, format='JPEG')
-    return img_byte_arr.getvalue()
+    img_base64 = base64.b64encode(img_byte_arr.getvalue()).decode()
+    return img_base64
 
 @pytest.fixture
 def api_instance():
@@ -44,19 +53,19 @@ def api_instance():
     return api
 
 # Test for correct classification output
-def test_classification_output(api_instance, test_image):
+def test_classification_output(api_instance, base64_test_image):
     # Mock CherryPy request
     cherrypy.request = MagicMock()
-    cherrypy.request.body.fp = io.BytesIO(test_image)
+    cherrypy.request.json = {"image_base64": base64_test_image}
 
     # Call classify_image
     response = api_instance.classify_image()
 
     # Check if there is a 'label_scores' key in the response
-    assert 'label_scores' in response, "No label_scores in the response"
+    assert 'predictions' in response, "No label_scores in the response"
 
     # Check if the label_scores dictionary is not empty
-    assert response['label_scores'], "The label_scores dictionary is empty"
+    assert response['predictions'], "The label_scores dictionary is empty"
 
 # Test to check for invalid image output 
 def test_invalid_image_input(api_instance):
@@ -65,7 +74,7 @@ def test_invalid_image_input(api_instance):
 
     # Mock CherryPy request
     cherrypy.request = MagicMock()
-    cherrypy.request.body.fp = io.BytesIO(invalid_image)
+    cherrypy.request.json = {"image_base64": io.BytesIO(invalid_image)}
 
     # Call classify_image
     response = api_instance.classify_image()
@@ -74,10 +83,10 @@ def test_invalid_image_input(api_instance):
     assert 'error' in response, "No error indicated for invalid image input"
 
 
-def test_response_structure(api_instance, test_image):
+def test_response_structure(api_instance, base64_test_image):
     # Mock CherryPy request
     cherrypy.request = MagicMock()
-    cherrypy.request.body.fp = io.BytesIO(test_image)
+    cherrypy.request.json = {"image_base64": base64_test_image}
 
     # Call classify_image
     response = api_instance.classify_image()
@@ -85,7 +94,7 @@ def test_response_structure(api_instance, test_image):
     # Check response structure
     assert isinstance(response, dict), "Response is not a dictionary"
     assert 'original_image' in response, "No original_image in the response"
-    assert 'label_scores' in response, "No label_scores in the response"
+    assert 'predictions' in response, "No label_scores in the response"
 
 
 def test_large_image_input(api_instance):
@@ -93,28 +102,16 @@ def test_large_image_input(api_instance):
     large_img = Image.new('RGB', (4000, 4000), color='blue')
     img_byte_arr = io.BytesIO()
     large_img.save(img_byte_arr, format='JPEG')
-    large_image = img_byte_arr.getvalue()
+    img_base64 = base64.b64encode(img_byte_arr.getvalue()).decode()
 
     # Mock CherryPy request
     cherrypy.request = MagicMock()
-    cherrypy.request.body.fp = io.BytesIO(large_image)
+    cherrypy.request.json = {"image_base64": img_base64}
 
     # Call classify_image
     response = api_instance.classify_image()
 
     # Check if the API can handle large images
-    assert 'label_scores' in response, "API failed to handle large image"
-
-
-# def test_specific_model_output(api_instance, test_image):
-#     # Mock CherryPy request
-#     cherrypy.request = MagicMock()
-#     cherrypy.request.body.fp = io.BytesIO(test_image)
-
-#     # Call classify_image
-#     response = api_instance.classify_image()
-
-#     # Check for a specific expected output
-#     expected_label = "expected_label"
-#     assert expected_label in response['label_scores'], f"{expected_label} not in label scores"
-
+    assert isinstance(response, dict), "Response is not a dictionary"
+    assert 'original_image' in response, "No original_image in the response"
+    assert 'predictions' in response, "API failed to handle large image"
