d92884544db5d1f48b6cf6fac84c734b97ddd645
ixaxaar <root@ixaxaar.in>
2024-02-16 15:40:53 +0530

    refactor

--- geniusrise_vision/segment/api.py
+++ geniusrise_vision/segment/api.py
@@ -14,46 +14,19 @@
 # limitations under the License.
 
 from geniusrise import BatchInput, BatchOutput, State
-from geniusrise.logging import setup_logger
 from geniusrise_vision.base import VisionAPI
 import io
-import logging
 import numpy as np
-from PIL import Image
 import cherrypy
 import torch
 import base64
 from PIL import Image
-from transformers import AutoModelForUniversalSegmentation, AutoModelForSemanticSegmentation, AutoImageProcessor
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger(__name__)
 
 class VisionSegmentationAPI(VisionAPI):
-    def __init__(self, model_name: str, subtask: str, input: BatchInput, output: BatchOutput, state: State):
-        """
-        Initialize the API with a specified model.
-        """
+    def __init__(self, input: BatchInput, output: BatchOutput, state: State, **kwargs):
+        """ """
         super().__init__(input=input, output=output, state=state)
-         # Initialize model and processor
-        self.model_name = model_name
-        self.subtask = subtask
-        self.processor = AutoImageProcessor.from_pretrained(model_name)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        if subtask in {"panoptic", None} and hasattr(self.processor, "post_process_panoptic_segmentation") or model_name in {"facebook/mask2former-swin-small-ade-semantic", "facebook/maskformer-swin-base-ade"}:
-            self.model = AutoModelForUniversalSegmentation.from_pretrained(model_name)
-        elif subtask in {"instance", None} and hasattr(self.processor, "post_process_instance_segmentation"):
-            self.model = AutoModelForUniversalSegmentation.from_pretrained(model_name)
-        elif subtask in {"semantic", None} and hasattr(self.processor, "post_process_semantic_segmentation"):
-            self.model = AutoModelForSemanticSegmentation.from_pretrained(model_name)
-        else:
-            raise ValueError(f"Subtask {subtask} is not supported for model {type(self.model)}")
-
-        self.model.to(self.device)
-
-        log.info("Model and processor are initialized. API is ready to serve requests.")
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
@@ -66,16 +39,21 @@ class VisionSegmentationAPI(VisionAPI):
         try:
             data = cherrypy.request.json
             image_base64 = data.get("image_base64", "")
+            subtask = data.get("subtask", "")
+
+            kwargs = data
+            if "image_base64" in kwargs:
+                del kwargs["image_base64"]
+            if "subtask" in kwargs:
+                del kwargs["subtask"]
+
             image_bytes = base64.b64decode(image_base64)
             image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
 
-            target_size = [(image.height, image.width)]
+            # in some cases we need to feed the tokenized task as an input
             if self.model.config.__class__.__name__ == "OneFormerConfig":
-                if subtask is None:
-                    kwargs = {}
-                else:
-                    kwargs = {"task_inputs": [subtask]}
-                    inputs = self.processor(images=[image], return_tensors="pt", **kwargs)
+                if subtask is not None:
+                    inputs = self.processor(images=[image], return_tensors="pt", task_inputs=[subtask])
                     inputs["task_inputs"] = self.tokenizer(
                         inputs["task_inputs"],
                         padding="max_length",
@@ -84,24 +62,21 @@ class VisionSegmentationAPI(VisionAPI):
                     )["input_ids"]
             else:
                 inputs = self.processor(images=[image], return_tensors="pt")
-            inputs["target_size"] = target_size
 
+            target_size = [(image.height, image.width)]
+            # inputs["target_size"] = target_size
 
             with torch.no_grad():
-                target_size = inputs.pop("target_size")
+                # target_size = inputs.pop("target_size")
                 model_outputs = self.model(**inputs)
-                model_outputs["target_size"] = target_size
+                # model_outputs["target_size"] = target_size
 
-                fn = None
-                if self.subtask in {"panoptic", None} and hasattr(self.processor, "post_process_panoptic_segmentation"):
-                    fn = self.processor.post_process_panoptic_segmentation
-                elif self.subtask in {"instance", None} and hasattr(self.processor, "post_process_instance_segmentation"):
-                    fn = self.processor.post_process_instance_segmentation
-
-                if fn is not None:
-                    outputs = fn(
+                # Post-processing
+                # Panoptic segmentation
+                if subtask == "panoptic":
+                    outputs = self.processor.post_process_panoptic_segmentation(
                         model_outputs,
-                        target_sizes=model_outputs["target_size"],
+                        target_sizes=target_size,
                     )[0]
 
                     annotation = []
@@ -120,9 +95,33 @@ class VisionSegmentationAPI(VisionAPI):
 
                         annotation.append({"score": score, "label": label, "mask": image_base64})
 
-                elif self.subtask in {"semantic", None} and hasattr(self.processor, "post_process_semantic_segmentation"):
+                # Instance segmentation
+                elif subtask == "instance":
+                    outputs = self.processor.post_process_instance_segmentation(
+                        model_outputs,
+                        target_sizes=target_size,
+                    )[0]
+
+                    annotation = []
+                    segmentation = outputs["segmentation"]
+
+                    for segment in outputs["segments_info"]:
+                        mask = (segmentation == segment["id"]) * 255
+                        mask = Image.fromarray(mask.numpy().astype(np.uint8), mode="L")
+                        label = self.model.config.id2label[segment["label_id"]]
+                        score = segment["score"]
+
+                        # Convert the original PIL image to base64 for returning
+                        buffered = io.BytesIO()
+                        mask.save(buffered, format="JPEG")
+                        image_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+                        annotation.append({"score": score, "label": label, "mask": image_base64})
+
+                # Semantic segmentation
+                elif subtask == "semantic":
                     outputs = self.processor.post_process_semantic_segmentation(
-                        model_outputs, target_sizes=model_outputs["target_size"]
+                        model_outputs, target_sizes=target_size
                     )[0]
 
                     annotation = []
@@ -145,5 +144,5 @@ class VisionSegmentationAPI(VisionAPI):
             return annotation
 
         except Exception as e:
-            log.error(f"Error processing image: {e}")
-            return {"success": False, "error": str(e)}
+            self.log.exception(f"Error processing image: {e}")
+            raise e
