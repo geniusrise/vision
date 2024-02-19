c20987cf4562ae9588fb9ee6c81c4aa6573ae6f8
ixaxaar <root@ixaxaar.in>
2024-02-19 19:14:22 +0530

    better transformers

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
 
 setup(
     name="geniusrise-vision",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=requirements,
     python_requires=">=3.10",
